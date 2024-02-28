83a9055f7e3a56f0e7a081edff2068f5e3d3ffd7
aarti-soni <aartisoni32748@gmail.com>
2024-02-28 07:58:00 +0530

    [Added] Setup, Navbar,  News, NewItem Components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar/>
+    <News/>
   </React.StrictMode>
 );
 
